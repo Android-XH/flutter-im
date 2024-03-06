6ddf108784c19d8d65bbcc8250dd0c8195026d8e
xuhao <xuhao.0103@163.com>
2024-03-06 11:36:02 +0800

    初始化iOS代码

---
+++ ios/Classes/SyImSdkPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface SyImSdkPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/SyImSdkPlugin.m
@@ -0,0 +1,20 @@
+#import "SyImSdkPlugin.h"
+
+@implementation SyImSdkPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"sy_im_sdk"
+            binaryMessenger:[registrar messenger]];
+  SyImSdkPlugin* instance = [[SyImSdkPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
