90f36aef0af409ccbc0abe694457aa82c80a5dd6
xuhao <xuhao.0103@163.com>
2024-03-07 11:28:42 +0800

    提交会话管理初始化代码

---
+++ lib/channel/impl/sy_im_sdk_conversation_impl.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:sy_im_sdk/channel/sy_im_sdk_conversation_interface.dart';
+import 'package:sy_im_sdk/common/channel_common.dart';
+import 'package:sy_im_sdk/common/sy_client_method_common.dart';
+import 'package:sy_im_sdk/config/sy_options.dart';
+
+import '../../listener/sy_call_back.dart';
+import '../../manager/data/auth_info.dart';
+import '../../manager/data/conversation_info.dart';
+import '../sy_im_sdk_platform_interface.dart';
+
+/// An implementation of [SyImSdkPlatform] that uses method channels.
+class SyImSdkConversationImpl extends SyImSdkConversation {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel =
+      const MethodChannel(ChannelCommon.syClientMethodChannel);
+
+
+  @override
+  Future<void> getConversationList({required String token, required SyCallBack<List<ConversationInfo>> callback}) {
+    // TODO: implement getConversationList
+
+
+  }
+
+}
